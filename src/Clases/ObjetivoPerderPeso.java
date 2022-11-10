cb2ca8fcd7b18facde03b6590c84565cbe0b86ac
obian - manu gallardo <manu@manu.com>
2022-11-09 21:04:41 -0300

    Agregar ObjetivoPerderPeso

--- src/Clases/ObjetivoPerderPeso.java
+++ src/Clases/ObjetivoPerderPeso.java
@@ -1,10 +1,72 @@
 package Clases;
 
-public class ObjetivoPerderPeso extends ObjetivoStrategy {
+import Enums.Exigencia;
+import Enums.TipoMuscular;
+import ListaEjercicios.Ejercicio;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 
-    public Rutina crearRutina(String[] dias) {
-        return new Rutina(null);
+interface _calculadoraPesoIdeal {
+    int calcular(int peso, double altura, char sexo);
+}
+
+public abstract class ObjetivoPerderPeso extends ObjetivoStrategy {
+    // dias : 1 hora hasta 1 hora y 30 minutos
+    // ejercicios: aerobico mayor igual a 3 y ejercitacion muscular cualquiera
+    private final int _duracionMaxima = 90;
+
+    private final int _pesoIdeal;
+    private final int _pesoInicial;
+
+    public ObjetivoPerderPeso(int peso, double altura, char sexo, _calculadoraPesoIdeal calculadora) {
+        this._pesoIdeal = calculadora.calcular(peso, altura, sexo);
+        this._pesoInicial = peso;
     }
 
+
+    public TipoMuscular elegirGrupoMuscular() {
+        TipoMuscular[] values = TipoMuscular.values();
+        int length = values.length;
+        int randIndex = new Random().nextInt(length);
+        return values[randIndex];
+    }
+
+    public ArrayList<Ejercicio> elegirEjerciciosDisponibles(TipoMuscular tipo) {
+        ArrayList<Ejercicio> ejercicios = ListaEjercicios.ListaEjercicios.getEjercicios();
+        ArrayList<Ejercicio> ejerciciosDisponibles = new ArrayList<Ejercicio>();
+        for (Ejercicio ejercicio : ejercicios) {
+            var nivelAerobico = ejercicio.getNivelAerobico();
+            var tipoMuscular = ejercicio.getTipoMuscular();
+
+            if ((nivelAerobico >= 3) && (tipoMuscular == tipo)) {
+                ejerciciosDisponibles.add(ejercicio);
+            }
+        }
+        return ejerciciosDisponibles;
+    }
+
+    public Rutina crearRutina(String[] dias) {
+        var diasEjercicio = new ArrayList<DiaEjercicio>();
+        for (String dia : dias) {
+            var duracionActual = 0;
+            TipoMuscular tipo = elegirGrupoMuscular();
+            ArrayList<Ejercicio> ejerciciosDisponibles = elegirEjerciciosDisponibles(tipo);
+            ArrayList<EjercicioRutina> ejerciciosElegidos = new ArrayList<EjercicioRutina>();
+
+            for (Ejercicio ejercicio : ejerciciosDisponibles) {
+                if ((duracionActual + ejercicio.getDuracion()) > this._duracionMaxima) {
+                    break;
+                }
+
+                var ejercicioRutina = new EjercicioRutina(ejercicio, 4, 10, 15);
+                ejerciciosElegidos.add(ejercicioRutina);
+                duracionActual += ejercicio.getDuracion();
+            }
+
+            diasEjercicio.add(new DiaEjercicio(dia, ejerciciosElegidos));
+        }
+        return new Rutina(diasEjercicio);
+    }
 }
